3abe6b0ebe71fd9e7bd033cfa1c8cb22cc82855f
Fayeaz Ahmed <r10.ahmed@yahoo.com>
2024-12-04 22:03:37 +0600

    Added local env for loki url

--- start-local.sh
+++ start-local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mvn -f discoveryservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local > discoveryservice.log 2>&1 &
-mvn -f gatewayservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local > gatewayservice.log 2>&1 &
-mvn -f requestservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local > requestservice.log 2>&1 &
-mvn -f handlerservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local > handlerservice.log 2>&1 &
+LOKI_URL=http://locahost:3100/loki/api/v1/push mvn -f gatewayservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local > gatewayservice.log 2>&1 &
+LOKI_URL=http://locahost:3100/loki/api/v1/push mvn -f requestservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local > requestservice.log 2>&1 &
+LOKI_URL=http://locahost:3100/loki/api/v1/push mvn -f handlerservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local > handlerservice.log 2>&1 &
 tail -f discoveryservice.log gatewayservice.log requestservice.log handlerservice.log
