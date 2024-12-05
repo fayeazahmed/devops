99ff0da4d8fdf94215b2dc32a5cded60cd18a000
Fayeaz Ahmed <r10.ahmed@yahoo.com>
2024-12-05 12:36:10 +0600

    Added configurations for loki in dev and prod

--- start-local.bat
+++ start-local.bat
@@ -1,5 +1,5 @@
 @echo off
 start cmd /k "mvn -f discoveryservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local"
-start cmd /k "set LOKI_URL=http://loki:3100/loki/api/v1/push && mvn -f gatewayservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local"
-start cmd /k "set LOKI_URL=http://loki:3100/loki/api/v1/push && mvn -f requestservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local"
-start cmd /k "set LOKI_URL=http://loki:3100/loki/api/v1/push && mvn -f handlerservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local"
+start cmd /k "set LOKI_URL=http://localhost:3100/loki/api/v1/push && mvn -f gatewayservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local"
+start cmd /k "set LOKI_URL=http://localhost:3100/loki/api/v1/push && mvn -f requestservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local"
+start cmd /k "set LOKI_URL=http://localhost:3100/loki/api/v1/push && mvn -f handlerservice/pom.xml spring-boot:run -Dspring-boot.run.profiles=local"
